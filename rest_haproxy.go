0991374681441fc2e0b23f502ec795f3e0921626
malnick <malnick@gmail..com>
2015-07-31 12:18:46 -0700

    test

--- rest_haproxy.go
+++ rest_haproxy.go
@@ -22,8 +22,8 @@ func parsefile(filename string) (*Services, error) {
 	var backends []Backend
 
 	// Define our regex to parse
-	match_bkend, err := regexp.Compile(`^\s*server`)
-	match_srv, err := regexp.Compile(`^\s*backend`)
+	match_bkend, err := regexp.Compile(`^\s*backend`)
+	//	match_srv, err := regexp.Compile(`^\s*server`)
 
 	if err != nil {
 		return nil, err // there was a problem with the regular expression.
@@ -42,32 +42,32 @@ func parsefile(filename string) (*Services, error) {
 			log.Println("MATCHED BACKEND: ", line)
 			larry := strings.Fields(line)
 			log.Println("BACKEND: ", larry[1])
-			backend[larry[1]] = make(map[string]string)
+			name := larry[1]
+			backend := make(map[strings(name)]string)
 			backends = append(backends, backend)
 		}
-		if match_srv.MatchString(line) {
-			log.Println("MATCHED SERVER:\n", line)
-			//			larry := strings.Fields(line)
-			//			log.Println("LENGTH: ", len(larry))
-			//			dest := strings.Split(larry[2], ":")
-			//			ip := dest[0]
-			//			port := dest[1]
-			//			if len(larry) == 6 {
-			//				mgmt := larry[5]
-			//				log.Println("MGMT: ", mgmt)
-			//			} else {
-			//				mgmt := port
-			//				log.Println("MGMT: ", mgmt)
-			//			}
-			//			//endpoint := strings.Join([]strings{ip, ":", mgmt})
-			//			log.Println("IP: ", ip)
-			//			//log.Println("MGMT: ", mgmt)
-			//			log.Println("PORT: ", port)
-			//			//log.Println("ENDPOINT ", endpoint)
-			//			endpoints[e] = append(endpoints[e], b)
-			//		}
-			//	}
-		}
+		//			if match_srv.MatchString(line) {
+		//				log.Println("MATCHED SERVER:\n", line)
+		//					larry := strings.Fields(line)
+		//					log.Println("LENGTH: ", len(larry))
+		//					dest := strings.Split(larry[2], ":")
+		//					ip := dest[0]
+		//					port := dest[1]
+		//			if len(larry) == 6 {
+		//				mgmt := larry[5]
+		//				log.Println("MGMT: ", mgmt)
+		//			} else {
+		//				mgmt := port
+		//				log.Println("MGMT: ", mgmt)
+		//			}
+		//			//endpoint := strings.Join([]strings{ip, ":", mgmt})
+		//			log.Println("IP: ", ip)
+		//			//log.Println("MGMT: ", mgmt)
+		//			log.Println("PORT: ", port)
+		//			//log.Println("ENDPOINT ", endpoint)
+		//			endpoints[e] = append(endpoints[e], b)
+		//		}
+		//	}
 	}
 
 	return &Services{
