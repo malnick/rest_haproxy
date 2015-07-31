84bb0953dd9b736ad9c344e275340235888075af
malnick <malnick@gmail..com>
2015-07-31 13:38:40 -0700

    aahhhh

--- rest_haproxy.go
+++ rest_haproxy.go
@@ -14,14 +14,27 @@ type Services struct {
 	Available []map[string]string
 }
 
-func parsefile(filename string) (*Services, error) {
-	// Backend Array Defined
-	var backends []map[string]string
-
+func get_backend(line string) (name string) {
 	// Define our regex to parse
 	match_bkend, err := regexp.Compile(`^\s*backend`)
-	//	match_srv, err := regexp.Compile(`^\s*server`)
+	if match_bkend.MatchString(line) {
+		log.Println("MATCHED BACKEND: ", line)
+		larry := strings.Fields(line)
+		log.Println("BACKEND: ", larry[1])
+		name := larry[1]
+		return name
+		//backend := make(map[string]string)
+		//backend[name] = ""
+	}
+	return name
+}
 
+func parsefile(filename string) (*Services, error) {
+	// Backend Array Defined
+	var backends []map[string][]string
+
+	// Regex for Server
+	match_srv, err := regexp.Compile(`^\s*server`)
 	if err != nil {
 		return nil, err // there was a problem with the regular expression.
 	}
@@ -31,40 +44,37 @@ func parsefile(filename string) (*Services, error) {
 	scanner := bufio.NewScanner(inFile)
 	scanner.Split(bufio.ScanLines)
 
-	//	Loop:
 	for scanner.Scan() {
 		line := scanner.Text()
-		if match_bkend.MatchString(line) {
-			log.Println("MATCHED BACKEND: ", line)
+		current_backend := get_backend(line)
+		backend := make(map[string]map[string]string)
+
+		if match_srv.MatchString(line) {
+			log.Println("MATCHED SERVER:\n", line)
 			larry := strings.Fields(line)
-			log.Println("BACKEND: ", larry[1])
-			name := larry[1]
-			backend := make(map[string]string)
-			backend[name] = ""
+			log.Println("LENGTH: ", len(larry))
+			dest := strings.Split(larry[2], ":")
+			ip := dest[0]
+			port := dest[1]
+			if len(larry) == 6 {
+				mgmt := larry[5]
+				log.Println("MGMT: ", mgmt)
+			} else {
+				mgmt := dest[1]
+				log.Println("MGMT: ", mgmt)
+			}
+			//endpoint := strings.Join([]strings{ip, ":", mgmt})
+			log.Println("IP: ", ip)
+			log.Println("MGMT: ", mgmt)
+			log.Println("PORT: ", port)
+			//log.Println("ENDPOINT ", endpoint)
+			//endpoints[e] = append(endpoints[e], b)
+
+			backend[current_backend][ip] = ip
+			backend[current_backend][port] = port
+			backend[current_backend][mgmt] = mgmt
 			backends = append(backends, backend)
 		}
-		//			if match_srv.MatchString(line) {
-		//				log.Println("MATCHED SERVER:\n", line)
-		//					larry := strings.Fields(line)
-		//					log.Println("LENGTH: ", len(larry))
-		//					dest := strings.Split(larry[2], ":")
-		//					ip := dest[0]
-		//					port := dest[1]
-		//			if len(larry) == 6 {
-		//				mgmt := larry[5]
-		//				log.Println("MGMT: ", mgmt)
-		//			} else {
-		//				mgmt := port
-		//				log.Println("MGMT: ", mgmt)
-		//			}
-		//			//endpoint := strings.Join([]strings{ip, ":", mgmt})
-		//			log.Println("IP: ", ip)
-		//			//log.Println("MGMT: ", mgmt)
-		//			log.Println("PORT: ", port)
-		//			//log.Println("ENDPOINT ", endpoint)
-		//			endpoints[e] = append(endpoints[e], b)
-		//		}
-		//	}
 	}
 
 	return &Services{
